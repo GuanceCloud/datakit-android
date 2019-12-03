79464614d1ff1dbdb168bf56cfe5fd5f4635169f
huangdianhua <huangdianhua@jiagouyun.com>
2019-12-03 21:11:36 +0800

    SDK待完善

---
+++ ft-sdk/src/main/java/com/ft/sdk/garble/bean/OP.java
@@ -0,0 +1,14 @@
+package com.ft.sdk.garble.bean;
+
+/**
+ * BY huangDianHua
+ * DATE:2019-12-02 14:00
+ * Description:
+ */
+public enum OP {
+    LANC("lanc"),CLS("cls"),CLK("clk"),OPEN("open");
+    public String value;
+    OP(String value){
+        this.value = value;
+    }
+}
---
+++ ft-sdk/src/main/java/com/ft/sdk/garble/db/base/DataBaseCallBack.java
@@ -0,0 +1,12 @@
+package com.ft.sdk.garble.db.base;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * BY huangDianHua
+ * DATE:2019-12-02 16:02
+ * Description:
+ */
+public interface DataBaseCallBack {
+    void run(SQLiteDatabase db);
+}
