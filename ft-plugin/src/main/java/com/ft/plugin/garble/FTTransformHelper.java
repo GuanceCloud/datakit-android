af65d667bc4c49ef403b01f9457c8e83b7d13584
huangdianhua <huangdianhua@jiagouyun.com>
2019-12-03 21:10:50 +0800

    插件有待完善

---
+++ ft-plugin/src/main/java/com/ft/plugin/garble/FTTransformHelper.java
@@ -0,0 +1,14 @@
+package com.ft.plugin.garble;
+
+/**
+ * BY huangDianHua
+ * DATE:2019-12-03 16:40
+ * Description:
+ */
+public class FTTransformHelper {
+    public FTExtension extension;
+    public boolean isHookOnMethodEnter= true;
+    public FTTransformHelper(FTExtension extension){
+        this.extension = extension;
+    }
+}
