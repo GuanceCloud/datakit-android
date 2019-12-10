a12de02bafb454b55218b6ddd9e634653c3ca6c4
huangdianhua <huangdianhua@jiagouyun.com>
2019-12-10 16:03:29 +0800

    SDK上传数据逻辑

--- ft-sdk/src/main/java/com/ft/sdk/garble/bean/RecordData.java
+++ ft-sdk/src/main/java/com/ft/sdk/garble/bean/RecordData.java
@@ -2,6 +2,9 @@ package com.ft.sdk.garble.bean;
 
 import androidx.annotation.NonNull;
 
+import com.ft.sdk.garble.utils.LogUtils;
+import com.ft.sdk.garble.utils.Utils;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -27,13 +30,11 @@ public class RecordData {
     /**
      * 操作类型
      */
-    private OP op;
+    private String op;
     /**
      * 操作数据
      */
     private OpData opdata;
-
-    private String dbJson;
 
     public long getId() {
         return id;
@@ -67,11 +68,11 @@ public class RecordData {
         this.rpn = rpn;
     }
 
-    public OP getOp() {
+    public String getOp() {
         return op;
     }
 
-    public void setOp(OP op) {
+    public void setOp(String op) {
         this.op = op;
     }
 
@@ -83,26 +84,20 @@ public class RecordData {
         this.opdata = opdata;
     }
 
-    public void setDbJson(String dbJson) {
-        this.dbJson = dbJson;
-    }
 
     public String getJsonString() {
-        if(dbJson != null && !dbJson.isEmpty()){
-            return dbJson;
-        }
         JSONObject recordData = new JSONObject();
         try {
-            if(cpn != null) {
-                recordData.put("cnp", cpn);
+            if(!Utils.isNullOrEmpty(cpn)) {
+                recordData.put("cpn", cpn);
             }
 
-            if(rpn != null) {
+            if(!Utils.isNullOrEmpty(rpn)) {
                 recordData.put("rpn", rpn);
             }
 
-            if(op.value != null) {
-                recordData.put("op", op.value);
+            if(!Utils.isNullOrEmpty(op)) {
+                recordData.put("op", op);
             }
 
             if (opdata != null && !opdata.isEmpty()) {
@@ -114,12 +109,60 @@ public class RecordData {
         return recordData.toString();
     }
 
+    public void parseJsonToObj(String json){
+        try{
+            JSONObject jsonObject = new JSONObject(json);
+            cpn = jsonObject.optString("cpn");
+            rpn = jsonObject.optString("rpn");
+            op = jsonObject.optString("op");
+            JSONObject opData = jsonObject.optJSONObject("opdata");
+            if(opData != null) {
+                OpData opData1 = new OpData();
+                opData1.vtp = opData.optString("vtp");
+                opData1.field = opData.optString("field");
+                opdata = opData1;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public String composeUpdateData(){
+        StringBuffer sb = new StringBuffer();
+        if(!Utils.isNullOrEmpty(cpn)){
+            sb.append("current_page_name="+cpn+",");
+        }
+        if(!Utils.isNullOrEmpty(rpn)){
+            sb.append("root_page_name="+rpn+",");
+        }
+        if(opdata != null){
+            if(!Utils.isNullOrEmpty(opdata.vtp)){
+                sb.append("vtp="+opdata.vtp+",");
+            }
+            if(!Utils.isNullOrEmpty(opdata.field)){
+                sb.append("field="+opdata.field+",");
+            }
+        }
+        int index = sb.lastIndexOf(",");
+        if (index > 0 && index == sb.length()-1) {
+            sb.deleteCharAt(sb.length()-1);
+        }
+        if(sb.length()>0){
+            sb.insert(0,",");
+        }
+        sb.append(" ");
+        sb.append("event=\""+op+"\"");
+        sb.append(" ");
+        sb.append(time*1000000);
+        return sb.toString();
+    }
+
     @NonNull
     @Override
     public String toString() {
         return "RecordData[id="+id+
                 ",time="+time+
-                ",data="+(dbJson==null?getJsonString():dbJson)+
+                ",data="+getJsonString()+
                 "]";
     }
 
@@ -155,8 +198,12 @@ public class RecordData {
 
         public JSONObject getJson() throws JSONException {
             JSONObject opdata = new JSONObject();
-            opdata.put("vtp", vtp);
-            opdata.put("field",field);
+            if(!Utils.isNullOrEmpty(vtp)) {
+                opdata.put("vtp", vtp);
+            }
+            if(!Utils.isNullOrEmpty(field)) {
+                opdata.put("field", field);
+            }
             return opdata;
         }
     }
--- ft-sdk/src/main/java/com/ft/sdk/garble/http/HttpClient.java
+++ ft-sdk/src/main/java/com/ft/sdk/garble/http/HttpClient.java
@@ -1,7 +1,5 @@
 package com.ft.sdk.garble.http;
 
-import com.ft.sdk.FTSDKConfig;
-import com.ft.sdk.FTSDKInstall;
 import com.ft.sdk.garble.FTHttpConfig;
 import com.ft.sdk.garble.utils.LogUtils;
 
@@ -22,68 +20,72 @@ import java.util.Iterator;
  * Description:
  */
 public abstract class HttpClient {
-    protected final int SEND_OUT_TIME = 30*1000;
-    protected final int READ_OUT_TIME = 10*1000;
+    protected final int SEND_OUT_TIME = 30 * 1000;
+    protected final int READ_OUT_TIME = 10 * 1000;
     protected final String CHARSET = "UTF-8";
     protected final String CONTENT_TYPE = "text/plain";
     protected HttpBuilder mHttpBuilder;
     protected HttpURLConnection mConnection;
     protected boolean connSuccess = false;
     protected FTHttpConfig ftHttpConfig = FTHttpConfig.get();
-    public static HttpBuilder Builder(){
+
+    public static HttpBuilder Builder() {
         return new HttpBuilder();
     }
+
     protected abstract String getBodyContent();
-    public HttpClient(HttpBuilder httpBuilder){
+
+    public HttpClient(HttpBuilder httpBuilder) {
         this.mHttpBuilder = httpBuilder;
-        if(openConnection()) {
+        if (openConnection()) {
             setCommonParams();
             setHeadParams();
         }
     }
 
-    protected boolean openConnection(){
-        try{
+    protected boolean openConnection() {
+        try {
             String urlStr = ftHttpConfig.metricsUrl;
-            if(mHttpBuilder.getUrl() != null){
+            if (mHttpBuilder.getUrl() != null) {
                 urlStr = mHttpBuilder.getUrl();
             }
             String model = mHttpBuilder.getModel();
-            if(model != null && !model.isEmpty()){
-                urlStr = urlStr + "/" +model;
+            if (model != null && !model.isEmpty()) {
+                urlStr = urlStr + "/" + model;
             }
-            final URL url = new URL(urlStr+getQueryString());
+            final URL url = new URL(urlStr + getQueryString());
             mConnection = (HttpURLConnection) url.openConnection();
-            if(mConnection == null){
-                LogUtils.e(String.format("connect %s feature",url.toString()));
-            }else{
+            if (mConnection == null) {
+                LogUtils.e(String.format("connect %s feature", url.toString()));
+            } else {
                 connSuccess = true;
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return connSuccess;
     }
-    private String getQueryString(){
+
+    private String getQueryString() {
         StringBuffer sb = new StringBuffer();
         if (mHttpBuilder.getMethod() == RequestMethod.GET) {
-            HashMap<String,Object> param = mHttpBuilder.getParams();
+            HashMap<String, Object> param = mHttpBuilder.getParams();
             if (param != null) {
                 Iterator<String> keys = param.keySet().iterator();
-                while (keys.hasNext()){
+                while (keys.hasNext()) {
                     String key = keys.next();
-                    sb.append("&"+key+"="+param.get(key));
+                    sb.append("&" + key + "=" + param.get(key));
                 }
             }
         }
         return sb.toString();
     }
 
-    private void setCommonParams(){
-        if(mHttpBuilder.getSendOutTime() == 0){
+    private void setCommonParams() {
+        if (mHttpBuilder.getSendOutTime() == 0) {
             mHttpBuilder.setSendOutTime(SEND_OUT_TIME);
         }
-        if(mHttpBuilder.getReadOutTime() == 0){
+        if (mHttpBuilder.getReadOutTime() == 0) {
             mHttpBuilder.setReadOutTime(READ_OUT_TIME);
         }
 
@@ -96,24 +98,24 @@ public abstract class HttpClient {
         mConnection.setReadTimeout(mHttpBuilder.getReadOutTime());
     }
 
-    private void setHeadParams(){
-        mConnection.addRequestProperty("X-Datakit-UUID",ftHttpConfig.uuid);
-        mConnection.addRequestProperty("User-Agent",ftHttpConfig.userAgent);
-        mConnection.addRequestProperty("Accept-Language","zh-CN");
-        mConnection.addRequestProperty("Content-Type",CONTENT_TYPE+";charset="+CHARSET);
-        HashMap<String,String> headMap =  mHttpBuilder.getHeadParams();
+    private void setHeadParams() {
+        mConnection.addRequestProperty("X-Datakit-UUID", ftHttpConfig.uuid);
+        mConnection.addRequestProperty("User-Agent", ftHttpConfig.userAgent);
+        mConnection.addRequestProperty("Accept-Language", "zh-CN");
+        mConnection.addRequestProperty("Content-Type", CONTENT_TYPE + ";charset=" + CHARSET);
+        HashMap<String, String> headMap = mHttpBuilder.getHeadParams();
         Iterator<String> keys = headMap.keySet().iterator();
-        while (keys.hasNext()){
+        while (keys.hasNext()) {
             String key = keys.next();
-            mConnection.addRequestProperty(key,headMap.get(key));
+            mConnection.addRequestProperty(key, headMap.get(key));
         }
     }
 
-    public void execute(final HttpCallback httpCallback){
+    public void execute(final HttpCallback httpCallback) {
         request(httpCallback);
     }
 
-    private void request(HttpCallback httpCallback){
+    private void request(HttpCallback httpCallback) {
         RequestMethod method = mHttpBuilder.getMethod();
         boolean isDoInput = method == RequestMethod.POST;
         OutputStream outputStream = null;
@@ -122,66 +124,66 @@ public abstract class HttpClient {
         BufferedReader reader = null;
         String tempLine = null;
         StringBuffer resultBuffer = new StringBuffer();
-        if(isDoInput){
+        if (isDoInput) {
             mConnection.setDoOutput(true);
         }
         mConnection.setDoInput(true);
         mConnection.setUseCaches(false);
         try {
             mConnection.connect();
-            if(isDoInput){
+            if (isDoInput) {
                 outputStream = mConnection.getOutputStream();
                 outputStream.write(getBodyContent().getBytes(CHARSET));
                 outputStream.flush();
             }
             int responseCode = mConnection.getResponseCode();
-            if(responseCode >= 300){
+            if (responseCode >= 300) {
                 inputStream = mConnection.getErrorStream();
-                inputStreamReader = new InputStreamReader(inputStream,CHARSET);
+                inputStreamReader = new InputStreamReader(inputStream, CHARSET);
                 reader = new BufferedReader(inputStreamReader);
-                while ((tempLine = reader.readLine()) != null){
+                while ((tempLine = reader.readLine()) != null) {
                     resultBuffer.append(tempLine);
                 }
-            }else if(responseCode == HttpURLConnection.HTTP_OK){
+            } else if (responseCode == HttpURLConnection.HTTP_OK) {
                 inputStream = mConnection.getInputStream();
-                inputStreamReader = new InputStreamReader(inputStream,CHARSET);
+                inputStreamReader = new InputStreamReader(inputStream, CHARSET);
                 reader = new BufferedReader(inputStreamReader);
-                while ((tempLine = reader.readLine())!=null){
+                while ((tempLine = reader.readLine()) != null) {
                     resultBuffer.append(tempLine);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
-                if(outputStream != null) {
+                if (outputStream != null) {
                     outputStream.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
             try {
-                if(reader != null) {
+                if (reader != null) {
                     reader.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
             try {
-                if(inputStreamReader != null) {
+                if (inputStreamReader != null) {
                     inputStreamReader.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
             try {
-                if(inputStream != null) {
+                if (inputStream != null) {
                     inputStream.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        httpCallback.onComplete(resultBuffer.toString());
+        httpCallback.onComplete(new ResponseData(resultBuffer.toString()));
     }
 }
