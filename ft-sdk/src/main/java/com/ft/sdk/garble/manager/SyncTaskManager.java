9b57b39f6b018066c14abe680e465176a698b0d8
huangdianhua <huangdianhua@jiagouyun.com>
2019-12-11 17:52:19 +0800

    SDK

--- ft-sdk/src/main/java/com/ft/sdk/garble/manager/SyncTaskManager.java
+++ ft-sdk/src/main/java/com/ft/sdk/garble/manager/SyncTaskManager.java
@@ -1,23 +1,17 @@
 package com.ft.sdk.garble.manager;
 
-import android.content.Context;
-
-import com.ft.sdk.FTApplication;
 import com.ft.sdk.garble.bean.RecordData;
 import com.ft.sdk.garble.db.FTDBManager;
 import com.ft.sdk.garble.http.FTHttpClient;
 import com.ft.sdk.garble.http.HttpCallback;
 import com.ft.sdk.garble.http.RequestMethod;
 import com.ft.sdk.garble.http.ResponseData;
-import com.ft.sdk.garble.utils.DeviceUtils;
 import com.ft.sdk.garble.utils.LogUtils;
 import com.ft.sdk.garble.utils.ThreadPoolUtils;
 import com.ft.sdk.garble.utils.Utils;
 
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -73,7 +67,7 @@ public class SyncTaskManager {
                         LogUtils.d(">>>网络未连接<<<");
                         break;
                     }
-                    if(errorCount.get() >= CLOSE_TIME){
+                    if (errorCount.get() >= CLOSE_TIME) {
                         LogUtils.d(">>>连续同步失败5次，停止当前轮询同步<<<");
                         break;
                     }
@@ -100,15 +94,16 @@ public class SyncTaskManager {
         if (requestDatas == null || requestDatas.isEmpty()) {
             return;
         }
-        LogUtils.d("同步的数据" + requestDatas);
-        //TODO 添加网络请求
-        requestNet(requestDatas, new SyncCallback() {
+        SyncDataManager syncDataManager = new SyncDataManager();
+        String body = syncDataManager.getBodyContent(requestDatas);
+        LogUtils.d("同步的数据" + body);
+        requestNet(body, new SyncCallback() {
             @Override
             public void isSuccess(boolean isSuccess) {
-                if(isSuccess){
+                if (isSuccess) {
                     deleteLastQuery(requestDatas);
                     errorCount.set(0);
-                }else{
+                } else {
                     errorCount.getAndIncrement();
                 }
             }
@@ -128,79 +123,16 @@ public class SyncTaskManager {
         FTDBManager.get().delete(ids);
     }
 
-    private void requestNet(List<RecordData> requestDatas, final SyncCallback syncCallback) {
-        FTHttpClient.Builder().setMethod(RequestMethod.POST).setBodyString(getBodyContent(requestDatas)).execute(new HttpCallback<ResponseData>() {
-            @Override
-            public void onComplete(ResponseData result) {
-                syncCallback.isSuccess(result.getCode() == HttpURLConnection.HTTP_OK);
-            }
-        });
-    }
-
-    private String getBodyContent(List<RecordData> recordDatas) {
-        StringBuffer sb = new StringBuffer();
-        String device = parseHashToString(getDeviceInfo());
-        for (RecordData recordData : recordDatas) {
-            sb.append(device);
-            sb.append(recordData.composeUpdateData());
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
-
-    private String parseHashToString(HashMap<String, Object> param) {
-        StringBuffer sb = new StringBuffer();
-        if (param != null) {
-            Iterator<String> keys = param.keySet().iterator();
-            while (keys.hasNext()) {
-                String key = keys.next();
-                Object value = param.get(key);
-                if (keys.hasNext()) {
-                    if (value != null) {
-                        if(value instanceof String && ((String) value).isEmpty()){
-                            value = "null";
-                        }
-                        sb.append(key + "=" + value + ",");
-                    } else {
-                        sb.append(key + ",");
+    private void requestNet(String body, final SyncCallback syncCallback) {
+        FTHttpClient.Builder()
+                .setMethod(RequestMethod.POST)
+                .setBodyString(body)
+                .execute(new HttpCallback<ResponseData>() {
+                    @Override
+                    public void onComplete(ResponseData result) {
+                        syncCallback.isSuccess(result.getCode() == HttpURLConnection.HTTP_OK);
                     }
-                } else {
-                    if (value != null) {
-                        if(value instanceof String && ((String) value).isEmpty()){
-                            value = "null";
-                        }
-                        sb.append(key + "=" + value);
-                    } else {
-                        sb.append(key);
-                    }
-                }
-            }
-        }
-
-        return sb.toString();
-    }
-
-    /**
-     * 获取必要的设备信息
-     *
-     * @return
-     */
-    private HashMap<String, Object> getDeviceInfo() {
-        Context context = FTApplication.getApplication();
-        HashMap<String, Object> objectHashMap = new HashMap<>();
-        objectHashMap.put("mobile_tracker", null);
-        objectHashMap.put("device_uuid", DeviceUtils.getUuid(context));
-        objectHashMap.put("application_identifier", DeviceUtils.getApplicationId(context));
-        objectHashMap.put("application_name", DeviceUtils.getAppName(context));
-        objectHashMap.put("sdk_version", DeviceUtils.getSDKVersion());
-        objectHashMap.put("imei", DeviceUtils.getImei(context));
-        objectHashMap.put("os", DeviceUtils.getOSName());
-        objectHashMap.put("os_version", DeviceUtils.getOSVersion());
-        objectHashMap.put("device_band", DeviceUtils.getDeviceBand());
-        objectHashMap.put("device_model", DeviceUtils.getDeviceModel());
-        objectHashMap.put("display", DeviceUtils.getDisplay(context));
-        objectHashMap.put("carrier", DeviceUtils.getCarrier(context));
-        return objectHashMap;
+                });
     }
 
     interface SyncCallback {
