9306a6acaf92dc015992c9d6e77d0c7a19a898b9
huangdianhua <huangdianhua@jiagouyun.com>
2019-12-16 19:30:43 +0800

    部分测试

---
+++ ft-sdk/src/androidTest/java/com/ft/sdk/DeviceUtilsTest.java
@@ -0,0 +1,31 @@
+package com.ft.sdk;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.ft.sdk.garble.utils.DeviceUtils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * BY huangDianHua
+ * DATE:2019-12-16 18:40
+ * Description:
+ */
+public class DeviceUtilsTest {
+    private Context getContext(){
+        return InstrumentationRegistry.getInstrumentation().getTargetContext();
+    }
+
+    @Test
+    public void getSDKUUid(){
+        assertNotEquals("",DeviceUtils.getSDKUUid(getContext()));
+    }
+    @Test
+    public void getUuid(){
+        assertNotEquals("",DeviceUtils.getUuid(getContext()));
+    }
+}
