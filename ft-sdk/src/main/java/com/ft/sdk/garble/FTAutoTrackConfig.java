848fd8f10c307ed81b1a8fc9edb24c34e91b23ee
huangdianhua <huangdianhua@jiagouyun.com>
2019-12-20 18:19:16 +0800

    FT-SDK添加过滤条件

--- ft-sdk/src/main/java/com/ft/sdk/garble/FTAutoTrackConfig.java
+++ ft-sdk/src/main/java/com/ft/sdk/garble/FTAutoTrackConfig.java
@@ -1,6 +1,12 @@
 package com.ft.sdk.garble;
 
+import android.view.View;
+
+import com.ft.sdk.FTAutoTrackType;
 import com.ft.sdk.FTSDKConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * BY huangDianHua
@@ -9,7 +15,11 @@ import com.ft.sdk.FTSDKConfig;
  */
 public class FTAutoTrackConfig {
     private boolean autoTrack;
+    private int enableAutoTrackType;
+    private ArrayList<Integer> ignoreAutoTrackActivitys;
+    private ArrayList<Class<?>> ignoreAutoTrackViews;
     private static FTAutoTrackConfig instance;
+
 
     private FTAutoTrackConfig() {
     }
@@ -26,9 +36,94 @@ public class FTAutoTrackConfig {
             return;
         }
         autoTrack = ftsdkConfig.isAutoTrack();
+        enableAutoTrackType = ftsdkConfig.getEnableAutoTrackType();
+        addIgnoreAutoTrackActivity(ftsdkConfig.getIgnoreAutoTrackActivitys());
+        addIgnoreAutoTrackView(ftsdkConfig.getIgnoreAutoTrackViews());
     }
 
+    private void addIgnoreAutoTrackActivity(List<Class<?>> classes){
+        if(classes != null && !classes.isEmpty()){
+            if(ignoreAutoTrackActivitys == null){
+                ignoreAutoTrackActivitys = new ArrayList<>();
+            }
+            int hashCode;
+            for (Class<?> activity:classes){
+                hashCode = activity.hashCode();
+                if(!ignoreAutoTrackActivitys.contains(hashCode)){
+                    ignoreAutoTrackActivitys.add(hashCode);
+                }
+            }
+        }
+    }
+
+    private void addIgnoreAutoTrackView(List<Class<?>> classes){
+        if(classes != null && !classes.isEmpty()){
+            if(ignoreAutoTrackViews == null){
+                ignoreAutoTrackViews = new ArrayList<>();
+            }
+            for (Class<?> view:classes){
+                if(!ignoreAutoTrackViews.contains(view)){
+                    ignoreAutoTrackViews.add(view);
+                }
+            }
+        }
+    }
+    /**
+     * 是否忽略Activity
+     * @param activity
+     * @return
+     */
+    public boolean isIgnoreAutoTrackActivity(Class<?> activity){
+        if(activity == null){
+            return false;
+        }
+        if(ignoreAutoTrackActivitys != null && ignoreAutoTrackActivitys.contains(activity.hashCode())){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 是否开启自动埋点
+     * @return
+     */
     public boolean isAutoTrack() {
         return autoTrack;
+    }
+
+    /**
+     * 开启的自动埋点类型
+     * @param type
+     * @return
+     */
+    public boolean enableAutoTrackType(FTAutoTrackType type){
+        if((enableAutoTrackType|type.type) == enableAutoTrackType){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 判断 View 是否被忽略
+     *
+     * @param view View
+     * @return 是否被忽略
+     */
+    public boolean isViewIgnored(View view) {
+        try {
+            if (view == null) {
+                return false;
+            }
+            if (ignoreAutoTrackViews != null) {
+                for (Class<?> clazz : ignoreAutoTrackViews) {
+                    if (clazz.isAssignableFrom(view.getClass())) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
