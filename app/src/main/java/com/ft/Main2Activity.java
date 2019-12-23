a2e8cd1a35d5338a53dc82b5a49004cc18f29059
huangdianhua <huangdianhua@jiagouyun.com>
2019-12-23 16:13:00 +0800

    更新APP目录样例

---
+++ app/src/main/java/com/ft/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.ft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
