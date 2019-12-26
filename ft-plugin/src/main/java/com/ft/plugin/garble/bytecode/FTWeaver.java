5c5ee7958834a2d3ad55bc1f575fbeb0f6d64c54
huangdianhua <huangdianhua@jiagouyun.com>
2019-12-26 15:08:47 +0800

    添加使用的开源代码的来源和版权

--- ft-plugin/src/main/java/com/ft/plugin/garble/bytecode/FTWeaver.java
+++ ft-plugin/src/main/java/com/ft/plugin/garble/bytecode/FTWeaver.java
@@ -9,7 +9,7 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 
 /**
- * BY huangDianHua
+ * From https://github.com/Leaking/Hunter/blob/master/hunter-timing-plugin/src/main/groovy/com/quinn/hunter/plugin/timing/bytecode/TimingWeaver.java
  * DATE:2019-11-29 15:16
  * Description:
  */
