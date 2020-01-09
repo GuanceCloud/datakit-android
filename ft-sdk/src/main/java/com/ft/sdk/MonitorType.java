e51cf4b508dc44bd5d8d009f6625976d424ab84e
huangdianhua <huangdianhua@jiagouyun.com>
2020-01-09 18:38:06 +0800

    FT-SDK添加监控配置

---
+++ ft-sdk/src/main/java/com/ft/sdk/MonitorType.java
@@ -0,0 +1,17 @@
+package com.ft.sdk;
+
+/**
+ * BY huangDianHua
+ * DATE:2020-01-09 17:16
+ * Description:
+ */
+public class MonitorType {
+    public static int ALL = 1;
+    public static int BATTERY = 1<<1;
+    public static int MEMORY = 1<<2;
+    public static int CPU = 1<<3;
+    public static int GPU = 1<<4;
+    public static int NETWORK = 1<<5;
+    public static int CAMERA = 1<<6;
+    public static int LOCATION = 1<<7;
+}
