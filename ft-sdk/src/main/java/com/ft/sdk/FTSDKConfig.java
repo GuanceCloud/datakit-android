9ec6515882f5677de763b242bda030e889e2c14d
huangdianhua <huangdianhua@jiagouyun.com>
2020-01-14 14:23:04 +0800

    FT-SDK Camera像素获取时添加Camera2获取方式

--- ft-sdk/src/main/java/com/ft/sdk/FTSDKConfig.java
+++ ft-sdk/src/main/java/com/ft/sdk/FTSDKConfig.java
@@ -121,51 +121,101 @@ public class FTSDKConfig {
         return this;
     }
 
+    /**
+     * 是否使用 UseOAID 作为设备唯一识别号的替代字段
+     * @param useOAID
+     * @return
+     */
     public FTSDKConfig setUseOAID(boolean useOAID) {
         this.useOAID = useOAID;
         return this;
     }
 
+    /**
+     * 是否开启Debug，开启后将显示 SDK 运行日志
+     * @param debug
+     * @return
+     */
     public FTSDKConfig setDebug(boolean debug) {
         isDebug = debug;
         return this;
     }
 
+    /**
+     * 设置自动埋点的事件类别
+     * @param type
+     * @return
+     */
     public FTSDKConfig setEnableAutoTrackType(int type) {
         enableAutoTrackType = type;
         return this;
     }
 
+    /**
+     * 设置监控类别
+     * @param monitorType
+     * @return
+     */
     public FTSDKConfig setMonitorType(int monitorType) {
         this.monitorType = monitorType;
         return this;
     }
 
+    /**
+     * 是否需要绑定用户信息
+     * @param needBindUserVar
+     * @return
+     */
     public FTSDKConfig setNeedBindUser(boolean needBindUserVar){
         needBindUser = needBindUserVar;
         return this;
     }
 
+    /**
+     * 设置白名单（Activity，Fragment）
+     * @param classes
+     * @return
+     */
     public FTSDKConfig setWhiteActivityClasses(List<Class<?>> classes) {
         whiteActivityClass = classes;
         return this;
     }
 
+    /**
+     * 设置控件白名单
+     * @param classes
+     * @return
+     */
     public FTSDKConfig setWhiteViewClasses(List<Class<?>> classes) {
         whiteViewClass = classes;
         return this;
     }
 
+    /**
+     * 设置关闭的自动埋点事件类别
+     * @param type
+     * @return
+     */
     public FTSDKConfig setDisableAutoTrackType(int type) {
         disableAutoTrackType = type;
         return this;
     }
 
+    /**
+     * 设置黑名单（Acitivty，Fragment）
+     * @param classes
+     * @return
+     */
     public FTSDKConfig setBlackActivityClasses(List<Class<?>> classes) {
         blackActivityClass = classes;
         return this;
     }
 
+    /**
+     * 设置控件黑名单
+     * @param classes
+     * @return
+     */
     public FTSDKConfig setBlackViewClasses(List<Class<?>> classes) {
         blackViewClass = classes;
         return this;
---
+++ ft-sdk/src/main/java/com/ft/sdk/garble/bean/CameraPx.java
@@ -0,0 +1,27 @@
+package com.ft.sdk.garble.bean;
+
+import android.hardware.camera2.CameraCharacteristics;
+
+import androidx.annotation.NonNull;
+
+/**
+ * BY huangDianHua
+ * DATE:2020-01-14 13:39
+ * Description:
+ */
+public class CameraPx{
+    public int face;
+    public long px;
+    public String id;
+
+    @NonNull
+    @Override
+    public String toString() {
+        String pxStr = px==0?"N/A":""+px;
+        if(face == CameraCharacteristics.LENS_FACING_FRONT){
+            return "camera_front"+id+"_px="+pxStr+"万像素,";
+        }else{
+            return "camera_back"+id+"_px="+pxStr+"万像素,";
+        }
+    }
+}
