790cbf8d47e95919407d0c06e84698babc996cd2
huangdianhua <huangdianhua@jiagouyun.com>
2020-01-17 14:26:15 +0800

    FT-SDK添加获取CPU温度方法，添加获取电池一系列属性的方法，修改获取地址信息的字段为城市名

---
+++ ft-sdk/src/main/java/com/ft/sdk/garble/bean/BatteryBean.java
@@ -0,0 +1,122 @@
+package com.ft.sdk.garble.bean;
+
+/**
+ * BY huangDianHua
+ * DATE:2020-01-17 13:30
+ * Description:
+ */
+public class BatteryBean {
+    /**
+     * 电量
+     */
+    private double br;
+    /**
+     * 电池状态
+     */
+    private String status;
+
+    /**
+     * 电池充电状态
+     */
+    private String plugState;
+
+    /**
+     * 电池健康状态
+     */
+    private String health;
+
+    /**
+     * 是否有电池
+     */
+    private boolean present;
+    /**
+     * 电池制造技术
+     */
+    private String technology;
+    /**
+     * 电池温度
+     */
+    private double temperature;
+
+    /**
+     *  电池电压
+     */
+    private double voltage;
+
+    /**
+     * 电池电总量
+     */
+    private double power;
+
+    public double getBr() {
+        return br;
+    }
+
+    public void setBr(double br) {
+        this.br = br;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPlugState() {
+        return plugState;
+    }
+
+    public void setPlugState(String plugState) {
+        this.plugState = plugState;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public boolean isPresent() {
+        return present;
+    }
+
+    public void setPresent(boolean present) {
+        this.present = present;
+    }
+
+    public String getTechnology() {
+        return technology;
+    }
+
+    public void setTechnology(String technology) {
+        this.technology = technology;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getVoltage() {
+        return voltage;
+    }
+
+    public void setVoltage(double voltage) {
+        this.voltage = voltage;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+}
--- ft-sdk/src/main/java/com/ft/sdk/garble/utils/BatteryUtils.java
+++ ft-sdk/src/main/java/com/ft/sdk/garble/utils/BatteryUtils.java
@@ -1,7 +1,11 @@
 package com.ft.sdk.garble.utils;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.BatteryManager;
+
+import com.ft.sdk.garble.bean.BatteryBean;
 
 /**
  * BY huangDianHua
@@ -10,6 +14,7 @@ import android.os.BatteryManager;
  */
 public class BatteryUtils {
     static double batteryCapacity = 0; //电池的容量mAh
+
     /**
      * 获取当前电量百分比
      *
@@ -34,7 +39,7 @@ public class BatteryUtils {
      * @return
      */
     public static double getBatteryTotal(Context context) {
-        if(batteryCapacity > 0){
+        if (batteryCapacity > 0) {
             return batteryCapacity;
         }
         Object mPowerProfile;
@@ -46,5 +51,107 @@ public class BatteryUtils {
             e.printStackTrace();
         }
         return batteryCapacity;
+    }
+
+    /**
+     * 获得电池信息
+     *
+     * @return
+     */
+    public static BatteryBean getBatteryInfo(Context context) {
+        BatteryBean batteryBean = new BatteryBean();
+        try {
+            Intent batteryStatus = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            if (batteryStatus != null) {
+                int temperature = batteryStatus.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1);
+                int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+                int plugState = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+                int health = batteryStatus.getIntExtra(BatteryManager.EXTRA_HEALTH, -1);
+                boolean present = batteryStatus.getBooleanExtra(BatteryManager.EXTRA_PRESENT, false);
+                String technology = batteryStatus.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY);
+                int voltage = batteryStatus.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1);
+                batteryBean.setStatus(batteryStatus(status));
+                batteryBean.setTemperature(temperature / 10);
+                batteryBean.setPlugState(batteryPlugged(plugState));
+                batteryBean.setHealth(batteryHealth(health));
+                batteryBean.setPresent(present);
+                batteryBean.setTechnology(technology);
+                if (voltage > 1000) {
+                    batteryBean.setVoltage(voltage / 1000f);
+                } else {
+                    batteryBean.setVoltage(voltage);
+                }
+                batteryBean.setPower(getBatteryTotal(context));
+                batteryBean.setBr(getBatteryCurrent(context));
+            }
+        } catch (Exception e) {
+        }
+        return batteryBean;
+    }
+
+    private static String batteryHealth(int health) {
+        String healthBat = Constants.UNKNOWN;
+        switch (health) {
+            case BatteryManager.BATTERY_HEALTH_COLD:
+                healthBat = "cold";
+                break;
+            case BatteryManager.BATTERY_HEALTH_DEAD:
+                healthBat = "dead";
+                break;
+            case BatteryManager.BATTERY_HEALTH_GOOD:
+                healthBat = "good";
+                break;
+            case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                healthBat = "overVoltage";
+                break;
+            case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                healthBat = "overheat";
+                break;
+            case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                healthBat = "unknown";
+                break;
+            case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                healthBat = "unspecified";
+                break;
+        }
+        return healthBat;
+    }
+
+    private static String batteryStatus(int status) {
+        String healthBat = Constants.UNKNOWN;
+        switch (status) {
+            case BatteryManager.BATTERY_STATUS_CHARGING:
+                healthBat = "charging";
+                break;
+            case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                healthBat = "disCharging";
+                break;
+            case BatteryManager.BATTERY_STATUS_FULL:
+                healthBat = "full";
+                break;
+            case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                healthBat = "notCharging";
+                break;
+            case BatteryManager.BATTERY_STATUS_UNKNOWN:
+                healthBat = "unknown";
+                break;
+        }
+        return healthBat;
+    }
+
+    private static String batteryPlugged(int status) {
+        String healthBat = Constants.UNKNOWN;
+        switch (status) {
+            case BatteryManager.BATTERY_PLUGGED_AC:
+                healthBat = "ac";
+                break;
+            case BatteryManager.BATTERY_PLUGGED_USB:
+                healthBat = "usb";
+                break;
+            case BatteryManager.BATTERY_PLUGGED_WIRELESS:
+                healthBat = "wireless";
+                break;
+        }
+        return healthBat;
     }
 }
