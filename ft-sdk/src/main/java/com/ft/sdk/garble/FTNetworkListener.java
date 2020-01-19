f30364e07ef832ec50e6fa208eb6c6cc9f58cec8
huangdianhua <huangdianhua@jiagouyun.com>
2020-01-19 18:59:11 +0800

    FT-SDK 网络状态监听

---
+++ ft-sdk/src/main/java/com/ft/sdk/garble/FTNetworkListener.java
@@ -0,0 +1,104 @@
+package com.ft.sdk.garble;
+
+import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkRequest;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+
+import com.ft.sdk.FTApplication;
+import com.ft.sdk.garble.manager.SyncTaskManager;
+import com.ft.sdk.garble.utils.LogUtils;
+import com.ft.sdk.garble.utils.NetUtils;
+
+/**
+ * BY huangDianHua
+ * DATE:2020-01-19 17:35
+ * Description: 监听网络连接状态
+ */
+public class FTNetworkListener {
+    private static FTNetworkListener instance;
+    private Application application;
+    private FTNetWorkCallback networkCallback;
+    private FTNetworkReceiver networkReceiver;
+    ConnectivityManager connectivityManager;
+
+    private FTNetworkListener() {
+    }
+
+    public synchronized static FTNetworkListener get() {
+        if (instance == null) {
+            instance = new FTNetworkListener();
+        }
+        return instance;
+    }
+
+    public void monitor() {
+        if (networkCallback == null) {
+            networkCallback = new FTNetWorkCallback();
+        }
+        if (application == null) {
+            application = FTApplication.getApplication();
+        }
+        if (networkReceiver == null) {
+            networkReceiver = new FTNetworkReceiver();
+        }
+        if (connectivityManager == null){
+            connectivityManager = (ConnectivityManager) application.getSystemService(Context.CONNECTIVITY_SERVICE);
+        }
+        initMonitor();
+    }
+
+    private void initMonitor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            connectivityManager.registerDefaultNetworkCallback(networkCallback);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            NetworkRequest.Builder builder = new NetworkRequest.Builder();
+            NetworkRequest request = builder.build();
+            connectivityManager.registerNetworkCallback(request,networkCallback);
+        } else {
+            IntentFilter intentFilter = new IntentFilter();
+            intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+            application.registerReceiver(networkReceiver,intentFilter);
+        }
+    }
+
+    public class FTNetWorkCallback extends ConnectivityManager.NetworkCallback {
+
+        @Override
+        public void onAvailable(@NonNull Network network) {
+            super.onAvailable(network);
+            judgeNetState();
+        }
+
+        @Override
+        public void onLost(@NonNull Network network) {
+            super.onLost(network);
+        }
+    }
+
+    public class FTNetworkReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            judgeNetState();
+        }
+    }
+
+    /**
+     * 判断网络是否可用
+     */
+    private void judgeNetState(){
+        //大于 0 有网
+        if (NetUtils.get().getNetworkState(application) > 0){
+            LogUtils.d("Net->"+"网络已连接");
+            SyncTaskManager.get().executeSyncPoll();
+        }
+    }
+}
