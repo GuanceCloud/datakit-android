85dbc11c0a3223bef04473d1d887550b547772be
huangdianhua <huangdianhua@jiagouyun.com>
2020-02-11 16:18:08 +0800

    修改常量引用和注释

--- ft-sdk/src/main/java/com/ft/sdk/garble/FTMonitorConfig.java
+++ ft-sdk/src/main/java/com/ft/sdk/garble/FTMonitorConfig.java
@@ -9,7 +9,7 @@ import com.ft.sdk.garble.utils.NetUtils;
 /**
  * BY huangDianHua
  * DATE:2020-01-09 17:26
- * Description:
+ * Description:监控项配置
  */
 public class FTMonitorConfig {
     private static FTMonitorConfig ftMonitorConfig;
@@ -26,15 +26,24 @@ public class FTMonitorConfig {
             mMonitorType = ftsdkConfig.getMonitorType();
         }
         if(isMonitorType(MonitorType.ALL)){
+            //开启网络监听
             NetUtils.get().listenerSignal(FTApplication.getApplication());
+            //开始获取地理位置
             LocationUtils.get().getCity();
         }else if(isMonitorType(MonitorType.NETWORK)){
+            //开启网络监听
             NetUtils.get().listenerSignal(FTApplication.getApplication());
         } else if (isMonitorType(MonitorType.LOCATION)) {
+            //获取地理位置
             LocationUtils.get().getCity();
         }
     }
 
+    /**
+     * 判断某种类型的监控是否开启
+     * @param monitorType
+     * @return
+     */
     public boolean isMonitorType(int monitorType){
         if(mMonitorType == 0){
             return false;
@@ -45,6 +54,9 @@ public class FTMonitorConfig {
         return false;
     }
 
+    /**
+     * 清楚当前监控配置项
+     */
     public void clear(){
         ftMonitorConfig = null;
     }
