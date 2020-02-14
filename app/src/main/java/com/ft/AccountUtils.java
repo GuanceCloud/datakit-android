a9d8b2f6a94b2031e36811ba4ac9b4de95593850
huangdianhua <huangdianhua@jiagouyun.com>
2020-02-14 17:31:49 +0800

    去除账号相关数据常量，账号相关数据放入本地配置文件中

---
+++ app/src/main/java/com/ft/AccountUtils.java
@@ -0,0 +1,46 @@
+package com.ft;
+
+import android.content.Context;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class AccountUtils {
+    //上传数据查询的登录账号
+    public final static String TEST_ACCOUNT = "TEST_ACCOUNT";
+    //上传数据查询的登录密码
+    public final static String TEST_PWD = "TEST_PWD";
+    //上传数据加密key
+    public final static String ACCESS_KEY_ID = "ACCESS_KEY_ID";
+    //上传数据加密密钥
+    public final static String ACCESS_KEY_SECRET = "ACCESS_KEY_SECRET";
+    //上传数据地址
+    public final static String ACCESS_SERVER_URL = "ACCESS_SERVER_URL";
+    /**
+     * 本地配置文件中读取登录的账户数据
+     */
+    public static String getProperty(Context context, String key) {
+        //testaccount.properties 该文件需要在资源目录assets中创建。如果本地没有需要自行创建，
+        //文件的内容为
+        //TEST_ACCOUNT = 你的账号
+        //TEST_PWD = 你的账号密码
+        Properties properties = new Properties();
+        InputStream inputStream = null;
+        try {
+            inputStream = context.getAssets().open("testaccount.properties");
+            properties.load(inputStream);
+            Enumeration en = properties.propertyNames();
+            while (en.hasMoreElements()) {
+                String keyVar = (String)en.nextElement();
+                if (key.contains(keyVar)) {
+                     return properties.getProperty(key);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
