5d75f0cf971ad639d06566f4212a253e8abd314e
huangdianhua <huangdianhua@jiagouyun.com>
2020-02-14 17:31:49 +0800

    去除账号相关数据常量，账号相关数据放入本地配置文件中

--- app/src/test/java/com/ft/Const.java
+++
@@ -1,7 +0,0 @@
-package com.ft;
-
-public class Const {
-    public static String accesskey_id = "accid";
-    public static String accessKey_secret = "accsk";
-    public static String serverUrl = "http://10.100.64.106:19457/v1/write/metrics";
-}
