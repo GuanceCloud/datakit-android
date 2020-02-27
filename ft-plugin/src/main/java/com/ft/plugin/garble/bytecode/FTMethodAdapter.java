993d73c104d20bc4a1c96924e90c64946ba0c161
huangdianhua <huangdianhua@jiagouyun.com>
2020-02-27 18:00:12 +0800

    添加使用Apache开源协议的文件所需要的说明

--- ft-plugin/src/main/java/com/ft/plugin/garble/bytecode/FTMethodAdapter.java
+++ ft-plugin/src/main/java/com/ft/plugin/garble/bytecode/FTMethodAdapter.java
@@ -1,3 +1,19 @@
+/*
+ * Created by wangzhuohou on 2015/08/01.
+ * Copyright 2015－2020 Sensors Data Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ft.plugin.garble.bytecode;
 
 import com.ft.plugin.garble.FTHookConfig;
@@ -17,9 +33,8 @@ import org.objectweb.asm.commons.AdviceAdapter;
 import java.util.HashMap;
 
 /**
- * BY huangDianHua
- * DATE:2019-11-29 15:04
- * Description:
+ * 本类借鉴修改了来自 Sensors Data 的项目 https://github.com/sensorsdata/sa-sdk-android-plugin2
+ * 中的 SensorsAnalyticsClassVisitor.groovy 类
  */
 public class FTMethodAdapter extends AdviceAdapter {
     private FTTransformHelper ftTransformHelper;
