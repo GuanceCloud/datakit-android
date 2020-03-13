c3eaca7da05d3d9d13cdd86c41f884935c730bd4
huangdianhua <huangdianhua@jiagouyun.com>
2020-03-13 14:18:17 +0800

    主动埋点，添加构造函数，参数添加判断

--- ft-sdk/src/main/java/com/ft/sdk/garble/bean/TrackBean.java
+++ ft-sdk/src/main/java/com/ft/sdk/garble/bean/TrackBean.java
@@ -7,16 +7,29 @@ import org.json.JSONObject;
  * time 2020/3/12 10:59:15
  */
 public class TrackBean {
-    String measurement;
-    JSONObject tags;
-    JSONObject fields;
-    long timeMillis;
+    private String measurement;
+    private JSONObject tags;
+    private JSONObject fields;
+    private long timeMillis;
 
     public TrackBean(String measurement, JSONObject tags, JSONObject fields, long timeMillis) {
         this.measurement = measurement;
         this.tags = tags;
         this.fields = fields;
         this.timeMillis = timeMillis;
+    }
+    public TrackBean(String measurement, JSONObject tags, JSONObject fields) {
+        this.measurement = measurement;
+        this.tags = tags;
+        this.fields = fields;
+        this.timeMillis = System.currentTimeMillis();
+    }
+
+    public TrackBean(String measurement, JSONObject fields) {
+        this.measurement = measurement;
+        this.tags = null;
+        this.fields = fields;
+        this.timeMillis = System.currentTimeMillis();
     }
 
     public String getMeasurement() {
