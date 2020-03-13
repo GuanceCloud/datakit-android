5b33460a9f548c9f1dfb64e33a218e86cf63bf39
huangdianhua <huangdianhua@jiagouyun.com>
2020-03-13 17:30:01 +0800

    子页面基本流程图统计基本完成（时间不对待改正）

--- ft-sdk/src/main/java/com/ft/sdk/garble/FTFragmentLifecycleCallbackDated.java
+++ ft-sdk/src/main/java/com/ft/sdk/garble/FTFragmentLifecycleCallbackDated.java
@@ -21,55 +21,34 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class FTFragmentLifecycleCallbackDated extends FragmentManager.FragmentLifecycleCallbacks {
     Activity activity;
-    ConcurrentLinkedQueue<Fragment> fragmentQueue;
+    FTFragmentLifecycleHandler fragmentLifecycleHandler;
     FTFragmentLifecycleCallbackDated(Activity activity){
         this.activity = activity;
-        fragmentQueue = new ConcurrentLinkedQueue<>();
+        fragmentLifecycleHandler = new FTFragmentLifecycleHandler(activity);
     }
     @Override
     public void onFragmentActivityCreated(FragmentManager fm, Fragment f, Bundle savedInstanceState) {
+        fragmentLifecycleHandler.onFragmentActivityCreated(f.getClass());
     }
 
     @Override
     public void onFragmentResumed(@NonNull FragmentManager fm, @NonNull Fragment f) {
-        if(FTFragmentManager.getInstance().ignoreFragments.contains(f.getClass().getName())){
-            return;
-        }
-        if(!f.getUserVisibleHint()){
-            return;
-        }
-        /**LogUtils.d("Fragment["+f.getClass().getSimpleName()+"] Dated state Resume");
-        Fragment fragment = fragmentQueue.poll();
-        String parent = Constants.FLOW_ROOT;
-        if(fragment != null){
-            parent = fragment.getClass().getSimpleName();
-        }
-        FTAutoTrack.startPage(f.getClass(), activity,parent);
-        fragmentQueue.add(f);*/
-        FTFragmentLifecycleHandler.getInstance().fragmentShow(f.getClass(),activity.getClass());
+        fragmentLifecycleHandler.onFragmentResumed(f.getClass());
     }
 
     @Override
     public void onFragmentPaused(@NonNull FragmentManager fm, @NonNull Fragment f) {
-        if(FTFragmentManager.getInstance().ignoreFragments.contains(f.getClass().getName())){
-            return;
-        }
-        if(!f.getUserVisibleHint()){
-            return;
-        }
-        //LogUtils.d("Fragment["+f.getClass().getSimpleName()+"] Dated state Pause");
-        //FTAutoTrack.destroyPage(f.getClass(), activity,Constants.FLOW_ROOT);
-
-        FTFragmentLifecycleHandler.getInstance().fragmentHidden(f.getClass(),activity.getClass());
+        fragmentLifecycleHandler.onFragmentPaused(f.getClass());
     }
 
     @Override
     public void onFragmentStopped(@NonNull FragmentManager fm, @NonNull Fragment f) {
+        fragmentLifecycleHandler.onFragmentStopped(f.getClass());
     }
 
     @Override
     public void onFragmentDestroyed(FragmentManager fm, Fragment f) {
-
+        fragmentLifecycleHandler.onFragmentDestroyed(f.getClass());
     }
 
     /**
@@ -78,10 +57,6 @@ public class FTFragmentLifecycleCallbackDated extends FragmentManager.FragmentLi
      * @param isVisible
      */
     public void setUserVisibleHint(Class fragment,boolean isVisible){
-        if(isVisible){
-            FTFragmentLifecycleHandler.getInstance().fragmentShow(fragment,activity.getClass());
-        }else{
-            FTFragmentLifecycleHandler.getInstance().fragmentHidden(fragment,activity.getClass());
-        }
+        fragmentLifecycleHandler.setUserVisibleHint(fragment,isVisible);
     }
 }
--- ft-sdk/src/main/java/com/ft/sdk/garble/FTFragmentLifecycleHandler.java
+++ ft-sdk/src/main/java/com/ft/sdk/garble/FTFragmentLifecycleHandler.java
@@ -1,6 +1,15 @@
 package com.ft.sdk.garble;
 
+
+import android.app.Activity;
+
+import com.ft.sdk.FTAutoTrack;
+import com.ft.sdk.garble.utils.Constants;
 import com.ft.sdk.garble.utils.LogUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * create: by huangDianHua
@@ -8,33 +17,96 @@ import com.ft.sdk.garble.utils.LogUtils;
  * description: Fragment 生命周期处理类
  */
 public class FTFragmentLifecycleHandler {
+    Activity activity;
+    List<Class> fragmentList;
+    ConcurrentHashMap<String, Boolean> fragmentUseVisibleHint;
 
-    private static FTFragmentLifecycleHandler instance;
-    private FTFragmentLifecycleHandler(){
-
+    FTFragmentLifecycleHandler(Activity activity) {
+        this.activity = activity;
+        fragmentList = new ArrayList<>();
+        fragmentUseVisibleHint = new ConcurrentHashMap<>();
     }
-    public static FTFragmentLifecycleHandler getInstance(){
-        synchronized (FTFragmentLifecycleHandler.class){
-            if(instance == null){
-                instance = new FTFragmentLifecycleHandler();
-            }
-            return instance;
+
+    public void onFragmentActivityCreated(Class fragment) {
+    }
+
+    public void onFragmentResumed(Class fragment) {
+        if (FTFragmentManager.getInstance().ignoreFragments.contains(fragment.getName())) {
+            return;
+        }
+        if (fragmentUseVisibleHint.containsKey(fragment.getName())) {
+            return;
+        }
+        LogUtils.d("Fragment[" + fragment.getSimpleName() + "] state Resume");
+        show(fragment);
+    }
+
+    public void onFragmentPaused(Class fragment) {
+        if (FTFragmentManager.getInstance().ignoreFragments.contains(fragment.getName())) {
+            return;
+        }
+        if (fragmentUseVisibleHint.containsKey(fragment.getName())) {
+            return;
+        }
+        LogUtils.d("Fragment[" + fragment.getSimpleName() + "] state Pause");
+        hidden(fragment);
+    }
+
+    public void onFragmentStopped(Class fragment) {
+    }
+
+    public void onFragmentDestroyed(Class fragment) {
+        fragmentUseVisibleHint.remove(fragment.getName());
+    }
+
+    /**
+     * 设置 Fragment 是否可见
+     *
+     * @param fragment
+     * @param isVisible
+     */
+    public void setUserVisibleHint(Class fragment, boolean isVisible) {
+        LogUtils.d("Fragment[" + fragment.getSimpleName() + "] isVisible=" + isVisible);
+        if (!fragmentUseVisibleHint.containsKey(fragment.getName())) {
+            fragmentUseVisibleHint.put(fragment.getName(), true);
+        }
+        if (isVisible) {
+            show(fragment);
+        } else {
+            hidden(fragment);
         }
     }
+
     /**
-     * Fragment 显示
      * @param fragment
      */
-    public void fragmentShow(Class fragment,Class activity){
-        //LogUtils.d("Fragment[\nshow=====>fragment:"+fragment.getSimpleName()+",activity:"+activity.getSimpleName());
+    private void show(Class fragment) {
+        Class clazz = getLastFragment();
+        String parent = Constants.FLOW_ROOT;
+        if (clazz != null) {
+            parent = clazz.getSimpleName();
+        }
+        FTAutoTrack.startPage(fragment, activity, parent);
+        fragmentList.add(fragment);
     }
 
     /**
-     * Fragment 隐藏
      * @param fragment
      */
-    public void fragmentHidden(Class fragment,Class activity){
-        //LogUtils.d("Fragment[\nhidden=====>fragment:"+fragment.getSimpleName()+",activity:"+activity.getSimpleName());
+    private void hidden(Class fragment) {
+        FTAutoTrack.destroyPage(fragment, activity, Constants.FLOW_ROOT);
     }
 
+    /**
+     * 得到父级 Fragment
+     *
+     * @return
+     */
+    Class getLastFragment() {
+        if (fragmentList.isEmpty()) {
+            return null;
+        } else {
+            return fragmentList.get(fragmentList.size() - 1);
+        }
+    }
 }
