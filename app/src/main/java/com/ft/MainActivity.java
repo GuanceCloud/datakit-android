163ffa39e519ec80ec371f1865cd6a183356e07b
huangdianhua <huangdianhua@jiagouyun.com>
2020-03-18 15:54:57 +0800

    冲突解决

--- app/src/main/java/com/ft/MainActivity.java
+++ app/src/main/java/com/ft/MainActivity.java
@@ -84,7 +84,6 @@ public class MainActivity extends AppCompatActivity {
         btn_lam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
             }
         });
         checkbox.setOnCheckedChangeListener((buttonView, isChecked) -> {
