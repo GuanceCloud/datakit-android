bb982415ebfc910895d312cb5e526e896452bc24
huangdianhua <huangdianhua@jiagouyun.com>
2020-03-19 16:51:00 +0800

    SDK 中网络请求返回错误结果规范

--- app/src/main/java/com/ft/Main2Activity.java
+++ app/src/main/java/com/ft/Main2Activity.java
@@ -1,8 +1,15 @@
 package com.ft;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.ft.sdk.FTTrack;
+import com.ft.sdk.garble.SyncCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Main2Activity extends AppCompatActivity {
@@ -13,7 +20,38 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         findViewById(R.id.jump).setOnClickListener(v -> {
+            trackImmediate();
         });
-        findViewById(R.id.jump2).setOnClickListener(v -> {});
+        findViewById(R.id.jump2).setOnClickListener(v -> {trackImmediateErr();});
+    }
+    public void trackImmediate(){
+        JSONObject field = new JSONObject();
+        try {
+            field.put("login","yes");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        FTTrack.getInstance().trackImmediate("mobile_tracker_artificial", null, field, new SyncCallback() {
+            @Override
+            public void onResponse(int code, String response) {
+                Log.d("onResponse","code="+code+",response="+response);
+            }
+        });
+    }
+
+    public void trackImmediateErr(){
+        FTTrack.getInstance().trackImmediate("mobile_tracker_artificial", null, null, new SyncCallback() {
+            @Override
+            public void onResponse(int code, String response) {
+                Log.d("onResponse","code="+code+",response="+response);
+            }
+        });
+
+        FTTrack.getInstance().trackImmediate(null, null, null, new SyncCallback() {
+            @Override
+            public void onResponse(int code, String response) {
+                Log.d("onResponse","code="+code+",response="+response);
+            }
+        });
     }
 }
