605fc4151ee17542ba1d00d61d356fb87ce70599
huangdianhua <huangdianhua@jiagouyun.com>
2020-03-20 16:57:54 +0800

    SDK 和 Plugin 中添加互相使用的最低版本限制

--- ft-plugin/src/main/java/com/ft/plugin/garble/bytecode/FTClassAdapter.java
+++ ft-plugin/src/main/java/com/ft/plugin/garble/bytecode/FTClassAdapter.java
@@ -16,7 +16,9 @@
  */
 package com.ft.plugin.garble.bytecode;
 
+import com.ft.plugin.garble.ClassNameAnalytics;
 import com.ft.plugin.garble.FTTransformHelper;
+import com.ft.plugin.garble.VersionConfig;
 
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.FieldVisitor;
@@ -52,6 +54,24 @@ public class FTClassAdapter extends ClassVisitor {
 
     @Override
     public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
+        if(ClassNameAnalytics.isFTSdkApi(className.replaceAll("/","."))){
+            if(name.equals("AGENT_VERSION")){
+                String agentVersion = (String) value;
+                if(!VersionConfig.firstVerGreaterEqual(agentVersion,VersionConfig.MIN_SDK_VERSION)){
+                   String errorTip = "你目前集成的 FT SDK 的版本为 "+agentVersion +",当前插件支持 SDK 的最低版本为 "+VersionConfig.MIN_SDK_VERSION
+                           +",详细信息请参考：https://github.com/CloudCare/dataflux-sdk-android";
+                   throw new Error(errorTip);
+                }
+            }
+            if(name.equals("PLUGIN_MIN_VERSION")){
+                String pluginMinVersion = (String) value;
+                if(!VersionConfig.firstVerGreaterEqual(VersionConfig.PLUGIN_VERSION,pluginMinVersion)){
+                   String errorTip = "你目前集成的 FT Plugin 的版本为 "+VersionConfig.PLUGIN_VERSION +",当前 SDK 支持的插件的最低版本为 "+pluginMinVersion
+                           +",详细信息请参考：https://github.com/CloudCare/dataflux-sdk-android";
+                   throw new Error(errorTip);
+                }
+            }
+        }
         return super.visitField(access, name, desc, signature, value);
     }
 
