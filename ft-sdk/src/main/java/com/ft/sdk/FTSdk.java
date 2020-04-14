2724f10c16f4b61c9ab7091a490b86b489ca1fc1
huangdianhua <huangdianhua@jiagouyun.com>
2020-04-14 11:20:13 +0800

    SDK 中提供一个及时定位并且获取结果的方法

--- ft-sdk/src/main/java/com/ft/sdk/FTSdk.java
+++ ft-sdk/src/main/java/com/ft/sdk/FTSdk.java
@@ -14,6 +14,7 @@ import com.ft.sdk.garble.FTHttpConfig;
 import com.ft.sdk.garble.FTMonitorConfig;
 import com.ft.sdk.garble.FTNetworkListener;
 import com.ft.sdk.garble.FTUserConfig;
+import com.ft.sdk.garble.SyncCallback;
 import com.ft.sdk.garble.manager.SyncTaskManager;
 import com.ft.sdk.garble.utils.GpuUtils;
 import com.ft.sdk.garble.utils.LocationUtils;
@@ -35,20 +36,22 @@ public class FTSdk {
     public static String PLUGIN_VERSION = "";
     //下面两个变量也不能随便改动，改动请同时更改 plugin 中对应的值
     public static final String AGENT_VERSION = BuildConfig.FT_SDK_VERSION;//当前SDK 版本
-    public static final String PLUGIN_MIN_VERSION  = BuildConfig.MIN_FT_PLUGIN_VERSION; //当前 SDK 支持的最小 Plugin 版本
+    public static final String PLUGIN_MIN_VERSION = BuildConfig.MIN_FT_PLUGIN_VERSION; //当前 SDK 支持的最小 Plugin 版本
     private static FTSdk mFtSdk;
     private FTSDKConfig mFtSDKConfig;
-    private FTSdk(@NonNull FTSDKConfig ftSDKConfig){
+
+    private FTSdk(@NonNull FTSDKConfig ftSDKConfig) {
         this.mFtSDKConfig = ftSDKConfig;
     }
 
     /**
      * SDK 配置项入口
+     *
      * @param ftSDKConfig
      * @return
      */
-    public static synchronized void install(@NonNull FTSDKConfig ftSDKConfig){
-        if (ftSDKConfig == null){
+    public static synchronized void install(@NonNull FTSDKConfig ftSDKConfig) {
+        if (ftSDKConfig == null) {
             throw new InvalidParameterException("ftSDKConfig 参数不能为 null");
         }
 
@@ -59,10 +62,11 @@ public class FTSdk {
 
     /**
      * SDK 初始化后，获得 SDK 对象
+     *
      * @return
      */
-    public static synchronized FTSdk get(){
-        if(mFtSdk == null){
+    public static synchronized FTSdk get() {
+        if (mFtSdk == null) {
             throw new InvalidParameterException("请先安装SDK(在应用启动时调用FTSdk.install(FTSDKConfig ftSdkConfig,Application application))");
         }
         return mFtSdk;
@@ -70,23 +74,25 @@ public class FTSdk {
 
     /**
      * 返回当前的 Application
+     *
      * @return
      */
-    public Application getApplication(){
+    public Application getApplication() {
         return FTApplication.getApplication();
     }
 
     /**
      * 关闭 SDK 正在做的操作
      */
-    public void shutDown(){
+    public void shutDown() {
         SyncTaskManager.get().shotDown();
     }
+
     /**
      * 注销用户信息
      */
-    public void unbindUserData(){
-        if(mFtSDKConfig != null){
+    public void unbindUserData() {
+        if (mFtSDKConfig != null) {
             if (mFtSDKConfig.isNeedBindUser()) {
                 LogUtils.d("解绑用户信息");
                 //解绑用户信息
@@ -101,42 +107,57 @@ public class FTSdk {
 
     /**
      * 绑定用户信息
+     *
      * @param name
      * @param id
      * @param extras
      */
-    public void bindUserData(@NonNull String name,@NonNull String id, JSONObject extras){
-        if(mFtSDKConfig != null){
-            if(mFtSDKConfig.isNeedBindUser()){
+    public void bindUserData(@NonNull String name, @NonNull String id, JSONObject extras) {
+        if (mFtSDKConfig != null) {
+            if (mFtSDKConfig.isNeedBindUser()) {
                 LogUtils.d("绑定用户信息");
                 //如果本地的SessionID已经绑定了用于就重新生成sessionId进行绑定
-                if(FTUserConfig.get().currentSessionHasUser()){
+                if (FTUserConfig.get().currentSessionHasUser()) {
                     FTUserConfig.get().clearSessionId();
                 }
                 //初始化SessionId
                 FTUserConfig.get().initSessionId();
                 //绑定用户信息
-                FTUserConfig.get().bindUserData(name,id,extras);
+                FTUserConfig.get().bindUserData(name, id, extras);
             }
         }
     }
 
     /**
      * 设置使用高德作为逆向地址解析
+     *
      * @param useGeoKey
      * @param geoKey
      * @return
      */
-    public void setGeoKey(boolean useGeoKey,String geoKey){
+    public void setGeoKey(boolean useGeoKey, String geoKey) {
         LocationUtils.get().setUseGeoKey(useGeoKey);
         LocationUtils.get().setGeoKey(geoKey);
+        startLocation(null, null);
+    }
+
+    /**
+     * 开启定，并且获取定位结果
+     */
+    public static void startLocation(String geoKey, SyncCallback syncCallback) {
+        if (geoKey != null) {
+            LocationUtils.get().setGeoKey(geoKey);
+            LocationUtils.get().setUseGeoKey(true);
+        }
+        LocationUtils.get().startLocationCallBack(FTApplication.getApplication(), syncCallback);
     }
 
     /**
      * 创建获取 GPU 信息的GLSurfaceView
+     *
      * @param root
      */
-    public void setGpuRenderer(ViewGroup root){
+    public void setGpuRenderer(ViewGroup root) {
         try {
             if (FTMonitorConfig.get().isMonitorType(MonitorType.GPU)) {
                 LogUtils.d("绑定视图监听 GPU 信息");
@@ -158,29 +179,29 @@ public class FTSdk {
                     }
                 });
             }
-        }catch (Exception e){
+        } catch (Exception e) {
         }
     }
 
     /**
      * 初始化SDK本地配置数据
      */
-    private void initFTConfig(){
-        if(mFtSDKConfig != null) {
+    private void initFTConfig() {
+        if (mFtSDKConfig != null) {
             LogUtils.setDebug(mFtSDKConfig.isDebug());
             FTHttpConfig.get().initParams(mFtSDKConfig);
             FTAutoTrackConfig.get().initParams(mFtSDKConfig);
             FTMonitorConfig.get().initParams(mFtSDKConfig);
             FTUserConfig.get().setNeedBindUser(mFtSDKConfig.isNeedBindUser());
-            if(mFtSDKConfig.isNeedBindUser()){
+            if (mFtSDKConfig.isNeedBindUser()) {
                 FTUserConfig.get().initSessionId();
                 FTUserConfig.get().initUserDataFromDB();
             }
             FTNetworkListener.get().monitor();
-            if(mFtSDKConfig.isAutoTrack()){
+            if (mFtSDKConfig.isAutoTrack()) {
                 trackStartApp();
             }
-            if(mFtSDKConfig.isOpenFlowChart()){
+            if (mFtSDKConfig.isOpenFlowChart()) {
                 FTFlowChartConfig.get().initParams(mFtSDKConfig);
             }
         }
@@ -189,15 +210,14 @@ public class FTSdk {
     /**
      * 添加 Activity 生命周期监控
      */
-    private void registerActivityLifeCallback(){
+    private void registerActivityLifeCallback() {
         FTActivityLifecycleCallbacks life = new FTActivityLifecycleCallbacks();
         getApplication().registerActivityLifecycleCallbacks(life);
     }
 
-    private void trackStartApp(){
+    private void trackStartApp() {
         FTAutoTrack.startApp();
     }
-
 
 
 }
