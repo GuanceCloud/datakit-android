bd5e202b134fb63e084c8d2a8bf2b21ba4bd6749
huangdianhua <huangdianhua@jiagouyun.com>
2020-04-28 14:21:51 +0800

    修改同步字段

--- app/src/main/java/com/ft/MainActivity.java
+++ app/src/main/java/com/ft/MainActivity.java
@@ -1,16 +1,10 @@
 package com.ft;
 
 import android.Manifest;
-import android.annotation.TargetApi;
-import android.bluetooth.BluetoothAdapter;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -37,21 +31,6 @@ import com.ft.sdk.FTSdk;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.net.NetworkInterface;
-import java.util.Collections;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -221,116 +200,4 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    private static final String marshmallowMacAddress = "02:00:00:00:00:00";
-    private static final String fileAddressMac = "/sys/class/net/wlan0/address";
-
-    public static String getAdresseMAC(Context context) {
-        WifiManager wifiMan = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-        WifiInfo wifiInf = wifiMan.getConnectionInfo();
-
-        if (wifiInf != null && marshmallowMacAddress.equals(wifiInf.getMacAddress())) {
-            String result = null;
-            try {
-                result = getAdressMacByInterface();
-                if (result != null) {
-                    return result;
-                } else {
-                    result = getAddressMacByFile(wifiMan);
-                    return result;
-                }
-            } catch (IOException e) {
-                Log.e("MobileAccess", "Erreur lecture propriete Adresse MAC");
-            } catch (Exception e) {
-                Log.e("MobileAcces", "Erreur lecture propriete Adresse MAC ");
-            }
-        } else {
-            if (wifiInf != null && wifiInf.getMacAddress() != null) {
-                return wifiInf.getMacAddress();
-            } else {
-                return "";
-            }
-        }
-        return marshmallowMacAddress;
-    }
-
-    private static String getAdressMacByInterface() {
-        try {
-            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
-            for (NetworkInterface nif : all) {
-                if (nif.getName().equalsIgnoreCase("wlan0")) {
-                    byte[] macBytes = nif.getHardwareAddress();
-                    if (macBytes == null) {
-                        return "";
-                    }
-
-                    StringBuilder res1 = new StringBuilder();
-                    for (byte b : macBytes) {
-                        res1.append(String.format("%02X:", b));
-                    }
-
-                    if (res1.length() > 0) {
-                        res1.deleteCharAt(res1.length() - 1);
-                    }
-                    return res1.toString();
-                }
-            }
-
-        } catch (Exception e) {
-            Log.e("MobileAcces", "Erreur lecture propriete Adresse MAC ");
-        }
-        return null;
-    }
-
-    private static String getAddressMacByFile(WifiManager wifiMan) throws Exception {
-        String ret;
-        int wifiState = wifiMan.getWifiState();
-
-        wifiMan.setWifiEnabled(true);
-        File fl = new File(fileAddressMac);
-        FileInputStream fin = new FileInputStream(fl);
-        ret = crunchifyGetStringFromStream(fin);
-        fin.close();
-
-        boolean enabled = WifiManager.WIFI_STATE_ENABLED == wifiState;
-        wifiMan.setWifiEnabled(enabled);
-        return ret;
-    }
-
-    private static String crunchifyGetStringFromStream(InputStream crunchifyStream) throws IOException {
-        if (crunchifyStream != null) {
-            Writer crunchifyWriter = new StringWriter();
-
-            char[] crunchifyBuffer = new char[2048];
-            try {
-                Reader crunchifyReader = new BufferedReader(new InputStreamReader(crunchifyStream, "UTF-8"));
-                int counter;
-                while ((counter = crunchifyReader.read(crunchifyBuffer)) != -1) {
-                    crunchifyWriter.write(crunchifyBuffer, 0, counter);
-                }
-            } finally {
-                crunchifyStream.close();
-            }
-            return crunchifyWriter.toString();
-        } else {
-            return "No Contents";
-        }
-    }
-
-    @TargetApi(23)
-    static String getBluetoothAddressSdk23(BluetoothAdapter adapter) {
-        if (adapter == null) return null;
-
-        Class<? extends BluetoothAdapter> btAdapterClass = adapter.getClass();
-        try {
-            Class<?> btClass = Class.forName("android.bluetooth.IBluetooth");
-            Field bluetooth = btAdapterClass.getDeclaredField("mService");
-            bluetooth.setAccessible(true);
-            Method btAddress = btClass.getMethod("getAddress");
-            btAddress.setAccessible(true);
-            return (String) btAddress.invoke(bluetooth.get(adapter));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return adapter.getAddress();
-        }
-    }
 }
