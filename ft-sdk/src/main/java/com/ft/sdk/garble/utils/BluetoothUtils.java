c454be34d03711f1812a76917c41acc36068195d
huangdianhua <huangdianhua@jiagouyun.com>
2020-04-28 19:02:39 +0800

    配对的蓝牙设备和蓝牙状态获取

--- ft-sdk/src/main/java/com/ft/sdk/garble/utils/BluetoothUtils.java
+++ ft-sdk/src/main/java/com/ft/sdk/garble/utils/BluetoothUtils.java
@@ -1,12 +1,19 @@
 package com.ft.sdk.garble.utils;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 
 import com.ft.sdk.FTApplication;
 
 import java.lang.reflect.Field;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 /**
  * create: by huangDianHua
@@ -31,6 +38,52 @@ public class BluetoothUtils {
     public String getDeviceName() {
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         return bluetoothAdapter.getName();
+    }
+
+    public boolean isOpen(){
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        return bluetoothAdapter.isEnabled();
+    }
+
+    public Set<BluetoothDevice> getBondedDevices(){
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        return bluetoothAdapter.getBondedDevices();
+    }
+
+    public void getBluetooth(){
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.enable();
+        }
+        //设置过滤器，过滤因远程蓝牙设备被找到而发送的广播 BluetoothDevice.ACTION_FOUND
+        IntentFilter iFilter=new IntentFilter();
+        iFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        //设置广播接收器和安装过滤器
+        FTApplication.getApplication().registerReceiver(new foundReceiver(), iFilter);
+        bluetoothAdapter.startDiscovery();
+    }
+
+    class foundReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // TODO Auto-generated method stub
+            try {
+                if (intent != null) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);//获取此时找到的远程设备对象
+                    if (device != null && !Utils.isNullOrEmpty(device.getName())) {
+                        short rssi = intent.getExtras().getShort(BluetoothDevice.EXTRA_RSSI);//获取额外rssi值
+                        LogUtils.d("BluetoothDevice-info:\n" +
+                                "name:" + device.getName() + "\n" +
+                                "rssi:" + rssi+"\n" +
+                                "address:"+device.getAddress());
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
     }
 
     /**
