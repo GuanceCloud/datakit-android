8241a6a44a8931dc760cd5509c0c2a69122abd0b
huangdianhua <huangdianhua@jiagouyun.com>
2020-06-16 14:57:22 +0800

    统计Activity的渲染时长

--- ft-sdk/src/main/java/com/ft/sdk/garble/bean/OP.java
+++ ft-sdk/src/main/java/com/ft/sdk/garble/bean/OP.java
@@ -6,8 +6,8 @@ package com.ft.sdk.garble.bean;
  * Description:
  */
 public enum OP {
-    LANC("lanc"),CLK("clk"),CSTM("cstm"),FLOW_CHAT("flow_chat")
-    ,CLS_ACT("cls_act"),OPEN_ACT("opn_act"),
+    LANC("lanc"),CLK("clk"),CSTM("cstm"),FLOW_CHAT("flow_chat"),OPEN("open"),
+    CLS_ACT("cls_act"),OPEN_ACT("opn_act"),
     CLS_FRA("cls_fra"),OPEN_FRA("open_fra"),LOG("log"),
     KEYEVENT("keyevent"),OBJECT("object");
     public String value;
