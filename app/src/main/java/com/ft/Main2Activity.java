ba07f07cf950db2f783dc3a6860f956c92799f8e
章博 <zhangbo@jiagouyun.com>
2020-07-06 14:27:04 +0800

    修改字段，添加各种网络 trace 的场景

--- app/src/main/java/com/ft/Main2Activity.java
+++ app/src/main/java/com/ft/Main2Activity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ft.sdk.FTAutoTrackType;
@@ -18,6 +19,7 @@ import com.ft.sdk.garble.bean.ObjectBean;
 import com.ft.sdk.garble.http.NetWorkTracerInterceptor;
 import com.ft.sdk.garble.http.RequestMethod;
 import com.ft.sdk.garble.utils.DeviceUtils;
+import com.ft.sdk.garble.utils.LogUtils;
 import com.ft.sdk.garble.utils.Utils;
 
 import org.json.JSONException;
@@ -148,62 +150,101 @@ public class Main2Activity extends AppCompatActivity {
         });
 
         findViewById(R.id.jump10).setOnClickListener(v -> {
-            new Thread() {
-                @Override
-                public void run() {
-                    super.run();
-                    String accessKey_id = "accid";
-                    String accessKey_secret = "accsk";
-
-                    Date currentTime = new Date();
-                    SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'", Locale.UK);
-                    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-                    String gmtString = sdf.format(currentTime);
-
-
-                    NetWorkTracerInterceptor interceptor = new NetWorkTracerInterceptor();
-                    OkHttpClient client = new OkHttpClient.Builder()
-                            .addInterceptor(interceptor)
-                            .build();
-
-
-                    long currentTimeNm = System.currentTimeMillis() * 1000000L;
-                    String body = "test_client," +
-                            "device_uuid=$deviceUUID," +
-                            "application_identifier=com.app.identifier," +
-                            "application_name=应用名称," +
-                            "sdk_version=1.0.0-alpha-6881c11," +
-                            "imei=123456789012347," +
-                            "os=Android," +
-                            "os_version=10," +
-                            "locale=zh_CN#Hans," +
-                            "device_band=Google," +
-                            "device_model=Pixel\\ 2," +
-                            "display=1980*1080," +
-                            "carrier=CDMA" +
-                            " event=\"launch\" " + currentTimeNm + "\n";
-
-                    String contentType = "text/plain; charset=utf-8";
-
-                    String sign = Utils.getHMacSha1(accessKey_secret, "POST" + "\n" + Utils.contentMD5Encode(body) + "\n" + contentType + "\n" + gmtString);
-                    String authorization = "DWAY " + accessKey_id + ":" + sign;
-                    Request.Builder builder = new Request.Builder().url("http://10.100.64.198:9528/v1/write/metrics?token=tkn_4c4f9f29f39c493199bb5abe7df6af21")
-                            .addHeader("Date", gmtString)
-                            .addHeader("Authorization", authorization)
-                            .addHeader("X-Datakit-UUID", DeviceUtils.getUuid(Main2Activity.this))
-                            .method(RequestMethod.POST.name(), RequestBody.create(null, body));
-
-
-                    try {
-                        Response response = client.newCall(builder.build()).execute();
-                        Log.i("test", Objects.requireNonNull(response.body()).string());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }.start();
+            requestUrl("https://www.baidu.com");
 
         });
+
+        findViewById(R.id.jump11).setOnClickListener(v -> {
+            requestUrl("https://error.url");
+        });
+
+        findViewById(R.id.jump12).setOnClickListener(v -> {
+            requestUrl("https://www.google.com");
+        });
+
+        findViewById(R.id.jump13).setOnClickListener(v -> {
+            requestUrl("https://www.baidu.com");
+            requestUrl("https://www.google.com");
+        });
+    }
+
+
+    private void requestUrl(@NonNull String url) {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                OkHttpClient client = new OkHttpClient.Builder()
+                        .addInterceptor(new NetWorkTracerInterceptor())
+                        .build();
+
+                Request.Builder builder = new Request.Builder().url(url)
+                        .method(RequestMethod.GET.name(), null);
+                try {
+                    client.newCall(builder.build()).execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    private void traceWithWriteMetrics() {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                String accessKey_id = "accid";
+                String accessKey_secret = "accsk";
+
+                Date currentTime = new Date();
+                SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'", Locale.UK);
+                sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+                String gmtString = sdf.format(currentTime);
+
+
+                NetWorkTracerInterceptor interceptor = new NetWorkTracerInterceptor();
+                OkHttpClient client = new OkHttpClient.Builder()
+                        .addInterceptor(interceptor)
+                        .build();
+
+
+                long currentTimeNm = System.currentTimeMillis() * 1000000L;
+                String body = "test_client," +
+                        "device_uuid=$deviceUUID," +
+                        "application_identifier=com.app.identifier," +
+                        "application_name=应用名称," +
+                        "sdk_version=1.0.0-alpha-6881c11," +
+                        "imei=123456789012347," +
+                        "os=Android," +
+                        "os_version=10," +
+                        "locale=zh_CN#Hans," +
+                        "device_band=Google," +
+                        "device_model=Pixel\\ 2," +
+                        "display=1980*1080," +
+                        "carrier=CDMA" +
+                        " event=\"launch\" " + currentTimeNm + "\n";
+
+                String contentType = "text/plain; charset=utf-8";
+
+                String sign = Utils.getHMacSha1(accessKey_secret, "POST" + "\n" + Utils.contentMD5Encode(body) + "\n" + contentType + "\n" + gmtString);
+                String authorization = "DWAY " + accessKey_id + ":" + sign;
+                Request.Builder builder = new Request.Builder().url("http://10.100.64.198:9528/v1/write/metrics?token=tkn_4c4f9f29f39c493199bb5abe7df6af21")
+                        .addHeader("Date", gmtString)
+                        .addHeader("Authorization", authorization)
+                        .addHeader("X-Datakit-UUID", DeviceUtils.getUuid(Main2Activity.this))
+                        .method(RequestMethod.POST.name(), RequestBody.create(null, body));
+
+
+                try {
+                    Response response = client.newCall(builder.build()).execute();
+                    LogUtils.i("test", Objects.requireNonNull(response.body()).string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
     }
 
     public void trackImmediate() {
