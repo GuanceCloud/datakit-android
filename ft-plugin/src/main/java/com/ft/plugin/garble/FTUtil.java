4c366b548032b617e0286a747a40b95019713159
huangdianhua <huangdianhua@jiagouyun.com>
2020-07-15 17:00:25 +0800

    升级 AGP 到4.0.1，插件中提示异常文件，优化插件

--- ft-plugin/src/main/java/com/ft/plugin/garble/FTUtil.java
+++ ft-plugin/src/main/java/com/ft/plugin/garble/FTUtil.java
@@ -30,7 +30,7 @@ import java.util.HashSet;
  * Description:
  */
 public class FTUtil {
-    public static final int ASM_VERSION = Opcodes.ASM6;
+    public static final int ASM_VERSION = Opcodes.ASM7;
     private static final HashSet<String> targetFragmentClass = new HashSet<>();
     private static final HashSet<String> targetXFragmentClass = new HashSet<>();
     private static final HashSet<String> targetV4FragmentClass = new HashSet<>();
