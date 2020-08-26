c90c7dd19cdb33428f5f43f8f84fdc14f8ecb5b6
huangdianhua <huangdianhua@jiagouyun.com>
2020-08-26 19:11:53 +0800

    页面事件日志测试用例，更新三方库版本

---
+++ app/src/androidTest/java/com/ft/Main2ActivityTest.java
@@ -0,0 +1,154 @@
+package com.ft;
+
+import android.content.Context;
+import android.os.Looper;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import com.ft.sdk.FTAutoTrackType;
+import com.ft.sdk.FTSDKConfig;
+import com.ft.sdk.FTSdk;
+import com.ft.sdk.MonitorType;
+import com.ft.sdk.TraceType;
+import com.ft.sdk.garble.bean.RecordData;
+import com.ft.sdk.garble.db.FTDBManager;
+import com.ft.sdk.garble.manager.SyncTaskManager;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * author: huangDianHua
+ * time: 2020/8/26 18:36:58
+ * description:
+ */
+@RunWith(AndroidJUnit4.class)
+public class Main2ActivityTest {
+    @Rule
+    public ActivityTestRule<Main2Activity> rule = new ActivityTestRule<>(Main2Activity.class);
+
+    Context context;
+    static boolean hasPrepare;
+
+    @Before
+    public void setUp() {
+        if (!hasPrepare) {
+            Looper.prepare();
+            hasPrepare = true;
+        }
+        context = DemoApplication.getContext();
+        FTSDKConfig ftSDKConfig = FTSDKConfig.builder(AccountUtils.getProperty(context, AccountUtils.ACCESS_SERVER_URL),
+                true,
+                AccountUtils.getProperty(context, AccountUtils.ACCESS_KEY_ID),
+                AccountUtils.getProperty(context, AccountUtils.ACCESS_KEY_SECRET))
+                .setDataWayToken(AccountUtils.getProperty(context, AccountUtils.ACCESS_SERVER_TOKEN))
+                .setXDataKitUUID("ft-dataKit-uuid-001")
+                .setUseOAID(true)//设置 OAID 是否可用
+                .setDebug(true)//设置是否是 debug
+                .setDescLog(true)
+                .enableAutoTrack(true)//设置是否开启自动埋点
+                .setEnableAutoTrackType(FTAutoTrackType.APP_CLICK.type |
+                        FTAutoTrackType.APP_END.type |
+                        FTAutoTrackType.APP_START.type)//设置埋点事件类型的白名单
+                .setGeoKey(true, AccountUtils.getProperty(context, AccountUtils.GEO_KEY))
+                .setNeedBindUser(false)//是否需要绑定用户信息
+                .setPageVtpDescEnabled(true)
+                .setMonitorType(MonitorType.ALL)//设置监控项
+                .trackNetRequestTime(true)
+                .setEnableTrackAppCrash(true)
+                .setEnv("dev")
+                .setTraceSamplingRate(0.5f)
+                .setNetworkTrace(true)
+                .setTraceConsoleLog(true)
+                .setEventFlowLog(true)
+                .setTraceType(TraceType.SKYWALKING_V2)
+                .setOnlySupportMainProcess(true);
+        FTSdk.install(ftSDKConfig);
+        //关闭数据自动同步操作
+        SyncTaskManager.get().setRunning(true);
+    }
+
+    /**
+     * 测试点击某个按钮
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void clickLambdaBtnTest() throws InterruptedException {
+        onView(withId(R.id.jump12)).perform(ViewActions.scrollTo()).perform(click());
+        //因为插入数据为异步操作，所以要设置一个间隔，以便能够查询到数据
+        Thread.sleep(1000);
+        List<RecordData> recordDataList = FTDBManager.get().queryDataByDescLimitLog(0);
+        boolean value = false;
+        for (RecordData recordData : recordDataList) {
+            if (recordData.toString().contains("jump12")) {
+                value = true;
+                break;
+            }
+        }
+        Assert.assertTrue(value);
+    }
+
+    @Test
+    public void leaveTest() throws InterruptedException {
+        //因为插入数据为异步操作，所以要设置一个间隔，以便能够查询到数据
+        Thread.sleep(1000);
+        List<RecordData> recordDataList = FTDBManager.get().queryDataByDescLimitLog(0);
+        boolean value = false;
+        for (RecordData recordData : recordDataList) {
+            if (recordData.toString().contains("leave")) {
+                value = true;
+                break;
+            }
+        }
+        Assert.assertTrue(value);
+    }
+
+    @Test
+    public void enterTest() throws InterruptedException {
+        //因为插入数据为异步操作，所以要设置一个间隔，以便能够查询到数据
+        Thread.sleep(1000);
+        List<RecordData> recordDataList = FTDBManager.get().queryDataByDescLimitLog(0);
+        boolean value = false;
+        for (RecordData recordData : recordDataList) {
+            System.out.println(recordData.toString());
+            if (recordData.toString().contains("enter")) {
+                value = true;
+                break;
+            }
+        }
+        Assert.assertTrue(value);
+    }
+
+    @Test
+    public void launchTest() throws InterruptedException {
+        //因为插入数据为异步操作，所以要设置一个间隔，以便能够查询到数据
+        Thread.sleep(1000);
+        List<RecordData> recordDataList = FTDBManager.get().queryDataByDescLimitLog(0);
+        boolean value = false;
+        for (RecordData recordData : recordDataList) {
+            if (recordData.toString().contains("launch")) {
+                value = true;
+                break;
+            }
+        }
+        Assert.assertTrue(value);
+    }
+
+    @After
+    public void end(){
+        FTDBManager.get().delete();
+    }
+}
