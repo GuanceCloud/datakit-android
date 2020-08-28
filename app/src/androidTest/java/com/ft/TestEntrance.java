4552e24b47efe26e222a316dbaf510b3b871476e
huangdianhua <huangdianhua@jiagouyun.com>
2020-08-28 17:13:12 +0800

    测试接口说明

--- app/src/androidTest/java/com/ft/TestEntrance.java
+++ app/src/androidTest/java/com/ft/TestEntrance.java
@@ -9,18 +9,20 @@ import org.junit.runners.Suite;
  * description:
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({BindUserTest.class,
-        FTAutoTrackTest.class,
-        FTInitParamTest.class,
-        LocationTest.class,
-        LogEventTest.class,
-        LogTest.class,
-        LogTrackObjectTraceTest.class,
-        MonitorTest.class,
-        OaidTest.class,
-        SDKRunStateTest.class,
-        TraceTest.class,
-        ExceptionTest.class})
+@Suite.SuiteClasses({
+        BindUserTest.class,//用户绑定测试
+        FTAutoTrackTest.class,//AutoTrack 测试
+        FTInitParamTest.class,//Base-Property 参数测试
+        LocationTest.class,//定位测试
+        LogEventTest.class,//Log-事件日志测试
+        LogTest.class,//Log-控制台日志,短时间批量插入丢弃策略测试
+        LogTrackObjectTraceTest.class,//数据同步-log、track、object、trace
+        MonitorTest.class,//监控测试
+        OaidTest.class,//oaid 测试
+        SDKRunStateTest.class,//SDK 生命周期测试
+        TraceTest.class,//trace 测试
+        ExceptionTest.class//Log-崩溃日志测试
+})
 public class TestEntrance {
     public static boolean hasPrepare;
 }
