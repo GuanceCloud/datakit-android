d895a529b4eee07cdb540932b6d3565b1dae1b94
章博 <brandonfaustine@gmail.com>
2020-11-20 15:57:59 +0800

    添加 UI 卡顿堆栈信息

--- ft-sdk/src/main/java/com/ft/sdk/garble/manager/FTUIBlockManager.java
+++ ft-sdk/src/main/java/com/ft/sdk/garble/manager/FTUIBlockManager.java
@@ -1,84 +1,58 @@
 package com.ft.sdk.garble.manager;
 
-import android.os.Looper;
+import android.view.Choreographer;
 
 import com.ft.sdk.FTAutoTrack;
 import com.ft.sdk.FTSDKConfig;
 import com.ft.sdk.garble.FTTrackInner;
 import com.ft.sdk.garble.bean.LogBean;
 import com.ft.sdk.garble.bean.Status;
-import com.ft.sdk.garble.interfaces.LooperLogPrinterListener;
-import com.ft.sdk.garble.interfaces.UiPerfMonitorConfig;
-import com.ft.sdk.garble.utils.LooperLogPrinter;
 
-/**
- * author: huangDianHua
- * time: 2020/9/28 10:45:00
- * description: UI 卡顿管理类
- */
-public class FTUIBlockManager implements LooperLogPrinterListener, UiPerfMonitorConfig {
-    public static final String TAG = "FTUIBlockManager";
-    private static FTUIBlockManager mInstance;
-    private LooperLogPrinter mLooperLogPrinter;
-    private int monitorState = UI_PERF_MONITOR_STOP;
-    private FTSDKConfig mConfig;
 
-    private FTUIBlockManager() {
-        mLooperLogPrinter = new LooperLogPrinter(this);
-    }
+public class FTUIBlockManager {
 
-    public synchronized static FTUIBlockManager getInstance() {
-        if (mInstance == null) {
-            mInstance = new FTUIBlockManager();
+    static Choreographer.FrameCallback callback = new Choreographer.FrameCallback() {
+
+        @Override
+        public void doFrame(long frameTimeNanos) {
+
+            FTMainLoopLogMonitor.getInstance().removeMonitor();
+
+            FTMainLoopLogMonitor.getInstance().startMonitor();
+
+            if (isStop) return;
+
+            Choreographer.getInstance().postFrameCallback(this);
         }
-        return mInstance;
-    }
+    };
 
-    public void startMonitor(FTSDKConfig config) {
+    private static boolean isStop = true;
+
+
+    public static void start(FTSDKConfig config) {
         if (!config.isEnableTrackAppUIBlock()) {
             return;
         }
-        Looper.getMainLooper().setMessageLogging(mLooperLogPrinter);
-        monitorState = UI_PERF_MONITOR_START;
-        mConfig = config;
-    }
 
-    public void stopMonitor() {
-        Looper.getMainLooper().setMessageLogging(null);
-        monitorState = UI_PERF_MONITOR_STOP;
-    }
+        isStop = false;
 
-    public boolean isMonitoring() {
-        return monitorState == UI_PERF_MONITOR_START;
-    }
+        FTMainLoopLogMonitor.getInstance().setLogCallBack(log -> {
+            FTAutoTrack.uiBlock();
 
-    @Override
-    public void onStartLoop() {
+            LogBean logBean = new LogBean("------ UIBlock  ------\n " + log, System.currentTimeMillis());
+            logBean.setStatus(Status.CRITICAL);
+            logBean.setEnv(config.getEnv());
+            logBean.setServiceName(config.getTraceServiceName());
+            FTTrackInner.getInstance().logBackground(logBean);
 
-    }
+        });
 
-    @Override
-    public void onEndLoop(String logInfo, int level) {
-        //卡顿等级可以自己定义
-        switch (level) {
-            case UI_PERF_LEVEL_1://处理超过 1 秒的卡顿
-            case UI_PERF_LEVEL_2://处理超过 1.5 秒的卡顿
-                FTAutoTrack.uiBlock();
 
-                LogBean logBean = new LogBean("------ UIBlock  ------, " + logInfo, System.currentTimeMillis());
-                logBean.setStatus(Status.CRITICAL);
-                logBean.setEnv(mConfig.getEnv());
-                logBean.setServiceName(mConfig.getTraceServiceName());
-                FTTrackInner.getInstance().logBackground(logBean);
-                break;
-
-        }
+        Choreographer.getInstance().postFrameCallback(callback);
     }
 
     public static void release() {
-        if (mInstance != null) {
-            mInstance.stopMonitor();
-            mInstance = null;
-        }
+        FTMainLoopLogMonitor.release();
+        isStop = true;
     }
 }
