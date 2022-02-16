10f1c836e2e122b0ab513210c12f6b1b35e535b3
章博 <brandonfaustine@gmail.com>
2022-02-16 16:26:26 +0800

    修改 trace addGlobalContext 测试用例

--- app/src/androidTest/java/com/ft/tests/TraceGlobalContextTest.java
+++ app/src/androidTest/java/com/ft/tests/TraceGlobalContextTest.java
@@ -1,6 +1,7 @@
 package com.ft.tests;
 
 import static com.ft.AllTests.hasPrepare;
+import static com.ft.utils.RequestUtil.requestUrl;
 
 import android.content.Context;
 import android.os.Looper;
@@ -13,9 +14,9 @@ import com.ft.BaseTest;
 import com.ft.DebugMainActivity;
 import com.ft.application.MockApplication;
 import com.ft.sdk.EnvType;
-import com.ft.sdk.FTRUMConfig;
 import com.ft.sdk.FTSDKConfig;
 import com.ft.sdk.FTSdk;
+import com.ft.sdk.FTTraceConfig;
 import com.ft.sdk.garble.bean.DataType;
 import com.ft.utils.CheckUtils;
 
@@ -50,17 +51,14 @@ public class TraceGlobalContextTest extends BaseTest {
                 .setEnv(EnvType.GRAY);
         FTSdk.install(ftSDKConfig);
 
-        FTSdk.initRUMWithConfig(new FTRUMConfig()
-                .setRumAppId(AccountUtils.getProperty(context, AccountUtils.RUM_APP_ID))
-                .addGlobalContext(CUSTOM_KEY, CUSTOM_VALUE)
-                .setEnableTraceUserAction(true)
-        );
+        FTSdk.initTraceWithConfig(new FTTraceConfig().addGlobalContext(CUSTOM_KEY, CUSTOM_VALUE));
 
     }
 
     @Test
     public void globalContextTest() throws Exception {
-        Thread.sleep(1000);
+        requestUrl("http://www.weather.com.cn/data/sk/101010100.html");
+        Thread.sleep(3000);
         Assert.assertTrue(CheckUtils.checkValue(DataType.TRACE,
                 new String[]{CUSTOM_KEY, CUSTOM_VALUE}, 0));
     }
